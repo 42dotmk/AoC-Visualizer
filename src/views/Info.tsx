d9c02dcedb9b94221b1890d548ca0d0a22d1241a
Ilija Boshkov <ilija@codechem.com>
2024-12-09 05:49:52 +0100

    ASCII

--- src/views/Info.tsx
+++ src/views/Info.tsx
@@ -1,3 +1,4 @@
+import { html } from "hono/html";
 import { Member } from "../types";
 import Music from "./Music";
 
@@ -14,13 +15,31 @@ const Info = ({ members }: { members: Member[] }) => {
         ))}
       </div>
 
-      <a href="https://42.mk">
-        <img
-          src="/base.svg"
-          className="m-0 w-[250px] h-[150px]"
-        />
+      <a class="no-underline text-xs" href="https://42.mk">
+        {html`
+          <pre id="ascii" class="bg-transparent m-0 p-0 overflow-visible">
+<span class="text-secondary-500"> MMMMMMMM         MMMMMMM         MMMMMMMMMM          MMMMMMMMM </span><span class="text-primary"> MM          MM     MMMMMMMMMM             </span>
+<span class="text-secondary-500">M      MMM      MM     MMMM      MM       MMMM      MM M        </span><span class="text-primary"> MM          MM   MMM         M            </span>
+<span class="text-secondary-500">M       MMMM    MM       MMM     MM         M MM  MMMM          </span><span class="text-primary"> MM          MM  MM           M            </span>
+<span class="text-secondary-500">M         MM    MM        MMMM   MM               MM            </span><span class="text-primary"> MM          MM               M            </span>
+<span class="text-secondary-500">M         MM    MM          MMM   MMM             MM            </span><span class="text-primary"> MMM         MM             MM             </span>
+<span class="text-secondary-500">M         MM    MM           MM     MMM           MM            </span><span class="text-primary">   MMM       MM           MMM              </span>
+<span class="text-secondary-500">MMMMMMMMMMMM    MM           MM       MM          MMMMMMM       </span><span class="text-primary">    MMMMMMMMMMM         MMM                </span>
+<span class="text-secondary-500">MMMMMMMMMM      MM           MM         MM        MM            </span><span class="text-primary">             MM       MMM                  </span>
+<span class="text-secondary-500">M        MMM    MMMMMMMMMMMMMMM           MM      MM            </span><span class="text-primary">             MM      MM                    </span>
+<span class="text-secondary-500">M         M MM  MMMMMMMMMMMMMMM            MMM    MM            </span><span class="text-primary">             MM    MM                      </span>
+<span class="text-secondary-500">M           MM  MM           MM  MM          MMM  MM            </span><span class="text-primary">             MM  MM           M            </span>
+<span class="text-secondary-500">M            M  MM           MM  MMM          MM  MMM           </span><span class="text-primary">             MM  M           MM            </span>
+<span class="text-secondary-500">M            M  MM           MM   MM M        MM   MMMM         </span><span class="text-primary">             MM  M         MMMM            </span>
+<span class="text-secondary-500">MMMMMMMMMMMMMM  MM           MM     MMMMMMMMMMMM     MMMMMMMMMM </span><span class="text-primary">             MM  MMMMMMMMMMMM              </span></pre>
+<script>
+  const ascii = document.getElementById("ascii");
+  const ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const getRandomChar = () => ALPHANUMERIC[Math.floor(Math.random() * ALPHANUMERIC.length)];
+  ascii.innerHTML = ascii.innerHTML.replace(/M/g, getRandomChar());
+</script>
+`}
       </a>
-
       <div className="mx-auto flex flex-col items-center content-center container max-w-4xl">
         <div>
           <p>
@@ -36,7 +55,8 @@ const Info = ({ members }: { members: Member[] }) => {
             <span className="font-bold text-xl shadow-primary text-primary underline">
               {allStars}
             </span>{" "}
-            stars in total. Keep up the good work!
+            stars in total in addition to the prize pool percentage. Keep up the
+            good work!
           </p>
           <p>
             This means we will donate{" "}
